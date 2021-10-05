93ff4d12395adf147068d8d4ae990b55e51b33b5
Hiich <raaanjnpmcn@gmail.com>
2021-10-05 16:10:24 +0800

    added regression training, exported model

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
 from joblib import load
-from src.schema import CvdRisk
+from src.schema import CVDRisk, BodyFat
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -17,14 +17,16 @@ app.add_middleware(
 )
 
 knn_clf_model = load('src/models/knn_clf_model.joblib') 
+ridge_regr_model = load('src/models/ridge_regr_model.joblib') 
 
 @app.get('/')
 def index():
     return {'message': 'Go to /docs and try out the model'}
 
 @app.post('/detect_cvd_risk')
-def detect_cvd_risk(data: CvdRisk):
+def detect_cvd_risk(data: CVDRisk):
     data = data.dict()
+    
     age = data['age']
     gender = data['gender']
     systolicBP = data['systolicBP']
@@ -35,7 +37,7 @@ def detect_cvd_risk(data: CvdRisk):
     alcoholic = data['alcoholic']
     active = data['active']
     bmi = data['bmi']
-    
+
     prediction = knn_clf_model.predict([[age, gender, systolicBP, diastolicBP, cholesterol, 
                                          glucose, smoke, alcoholic, active, bmi]])
     if(prediction[0] == 0):
@@ -48,6 +50,29 @@ def detect_cvd_risk(data: CvdRisk):
         'prediction': prediction,
         'result': result
     }
+
+@app.post('/predict_body_fat')
+def predict_body_fat(data: BodyFat):
+    data = data.dict()
+    
+    age = data['age']
+    weight = data['weight']
+    height = data['height']
+    neck = data['neck']
+    chest = data['chest']
+    abdomen = data['abdomen']
+    hip = data['hip']
+    thigh = data['thigh']
+    knee = data['knee']
+    ankle = data['ankle']
+    biceps = data['biceps']
+    forearm = data['forearm']
+    wrist = data['wrist']
+    
+    predictors = [age, weight, height, neck, chest, abdomen, hip, thigh, knee, ankle, biceps, forearm, wrist]
+    prediction = ridge_regr_model.predict([predictors])
+    
+    return {'prediction': prediction[0]}
     
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8000)
--- src/schema.py
+++ src/schema.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel, condecimal
 
-class CvdRisk(BaseModel):
+class CVDRisk(BaseModel):
     age: condecimal(decimal_places=0, ge=20, le=65)
     gender: condecimal(decimal_places=0, ge=0, le=1)
     systolicBP: condecimal(decimal_places=0, ge=90, le=200)
@@ -11,4 +11,19 @@ class CvdRisk(BaseModel):
     alcoholic: condecimal(decimal_places=0, ge=0, le=1)
     active: condecimal(decimal_places=0, ge=0, le=1)
     bmi: int
+
+class BodyFat(BaseModel):
+    age: condecimal(decimal_places=0, ge=20, le=80)
+    weight: condecimal(decimal_places=2, ge=120, le=365)
+    height: condecimal(decimal_places=2, ge=30, le=80)
+    neck: condecimal(decimal_places=2, ge=30, le=50)
+    chest: condecimal(decimal_places=2, ge=80, le=135)
+    abdomen: condecimal(decimal_places=2, ge=70, le=150)
+    hip: condecimal(decimal_places=2, ge=85, le=150)
+    thigh: condecimal(decimal_places=2, ge=50, le=90)
+    knee: condecimal(decimal_places=2, ge=35, le=50)
+    ankle: condecimal(decimal_places=2, ge=20, le=35)
+    biceps: condecimal(decimal_places=2, ge=25, le=45)
+    forearm: condecimal(decimal_places=2, ge=20, le=35)
+    wrist: condecimal(decimal_places=2, ge=15, le=20)
     

cdaf12c0f7495aa2de79303447ab6b6ecd66e27f
Hiich <raaanjnpmcn@gmail.com>
2021-10-06 23:23:21 +0800

    updated predictors array value

--- src/app.py
+++ src/app.py
@@ -1,5 +1,7 @@
 import uvicorn
+
 from fastapi import FastAPI
+
 from joblib import load
 from src.schema import CVDRisk, BodyFat
 from fastapi.middleware.cors import CORSMiddleware
@@ -20,58 +22,33 @@ knn_clf_model = load('src/models/knn_clf_model.joblib')
 ridge_regr_model = load('src/models/ridge_regr_model.joblib') 
 
 @app.get('/')
-def index():
+async def index():
     return {'message': 'Go to /docs and try out the model'}
 
 @app.post('/detect_cvd_risk')
-def detect_cvd_risk(data: CVDRisk):
+async def detect_cvd_risk(data: CVDRisk):
     data = data.dict()
-    
-    age = data['age']
-    gender = data['gender']
-    systolicBP = data['systolicBP']
-    diastolicBP = data['diastolicBP']
-    cholesterol = data['cholesterol']
-    glucose = data['glucose']
-    smoke = data['smoke']
-    alcoholic = data['alcoholic']
-    active = data['active']
-    bmi = data['bmi']
-
-    prediction = knn_clf_model.predict([[age, gender, systolicBP, diastolicBP, cholesterol, 
-                                         glucose, smoke, alcoholic, active, bmi]])
+    predictors = [data['age'], data['gender'], data['systolicBP'], data['diastolicBP'], data['cholesterol'], 
+                                         data['glucose'], data['smoke'], data['alcoholic'], data['active'], data['bmi']]
+    prediction = knn_clf_model.predict([predictors])
     if(prediction[0] == 0):
         prediction = "There's no risk of Cardiovascular Disease!"
         result = 0
     else:
         prediction = "There's a risk of Cardiovascular Disease."
         result = 1
+        
     return {
         'prediction': prediction,
         'result': result
     }
 
 @app.post('/predict_body_fat')
-def predict_body_fat(data: BodyFat):
-    data = data.dict()
-    
-    age = data['age']
-    weight = data['weight']
-    height = data['height']
-    neck = data['neck']
-    chest = data['chest']
-    abdomen = data['abdomen']
-    hip = data['hip']
-    thigh = data['thigh']
-    knee = data['knee']
-    ankle = data['ankle']
-    biceps = data['biceps']
-    forearm = data['forearm']
-    wrist = data['wrist']
-    
-    predictors = [age, weight, height, neck, chest, abdomen, hip, thigh, knee, ankle, biceps, forearm, wrist]
+async def predict_body_fat(data: BodyFat):
+    predictors = [data.age, data.weight, data.height, data.neck, data.chest, data.abdomen, data.hip, 
+                  data.thigh, data.knee, data.ankle, data.biceps, data.forearm, data.wrist]
     prediction = ridge_regr_model.predict([predictors])
-    
+
     return {'prediction': prediction[0]}
     
 if __name__ == '__main__':
